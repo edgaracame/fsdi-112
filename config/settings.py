a3da419043064cd1e1ab3a73e777a029601d04cb
Edgar Castillo <edgar101999.eacm@gmail.com>
2022-08-09 19:31:56 -0700

    Assignment 2: Tests and Heroku

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0u)@1iyq_9oygd09ya62e64ff)0ho(=jd+z0(6268pek2^@&kl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".herokuapp.com"]
 
 
 # Application definition
